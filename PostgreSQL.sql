16016a53265bd5fdc1bff12095eb65161447dc83
Nucifera Sambac <115205561+yznanee@users.noreply.github.com>
2022-12-20 15:39:56 +0700

    Update URL terkait PostgreSQL.sql

--- PostgreSQL.sql
+++ PostgreSQL.sql
@@ -1,4 +1,3 @@
 SQL kepanjangannya apa?
 SQL adalah singkatan dari Structured Query Language.
-http://sistem-komputer-s1.stekom.ac.id/informasi/baca/Apa-Itu-SQL-Pengertian-Jenis-Beserta-Fungsinya/b0be911eadc924612a478b7fe67e2d9f1b7d0bc9#:~:text=SQL%20adalah%20singkatan%20dari%20Structured,pengguna%20dalam%20mengakses%20data%20tersebut.
-
+http://sistem-komputer-s1.stekom.ac.id/informasi/baca/Apa-Itu-SQL-Pengertian-Jenis-Beserta-Fungsinya 
