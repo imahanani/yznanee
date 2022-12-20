815c987ac5bf5028d97226f51320774e7ac32537
Nucifera Sambac <115205561+yznanee@users.noreply.github.com>
2022-12-20 15:38:59 +0700

    SQL Pengerian dan URL terkait
    🎶 🦋

---
+++ PostgreSQL.sql
@@ -0,0 +1,4 @@
+SQL kepanjangannya apa?
+SQL adalah singkatan dari Structured Query Language.
+http://sistem-komputer-s1.stekom.ac.id/informasi/baca/Apa-Itu-SQL-Pengertian-Jenis-Beserta-Fungsinya/b0be911eadc924612a478b7fe67e2d9f1b7d0bc9#:~:text=SQL%20adalah%20singkatan%20dari%20Structured,pengguna%20dalam%20mengakses%20data%20tersebut.
+
