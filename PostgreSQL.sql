338ab1a945bdd764ef9fd363231c86e113530ccc
Nucifera Sambac <115205561+yznanee@users.noreply.github.com>
2022-12-21 12:43:07 +0700

    Fungsi DDL vs DML

--- PostgreSQL.sql
+++ PostgreSQL.sql
@@ -1,3 +1,6 @@
 SQL kepanjangannya apa?
 SQL adalah singkatan dari Structured Query Language.
 http://sistem-komputer-s1.stekom.ac.id/informasi/baca/Apa-Itu-SQL-Pengertian-Jenis-Beserta-Fungsinya 
+
+Perintah DDL : mendefinisikan atau membuat STRUKTUR BASIS DATA beserta tabelnya.
+Perintah DML : MEMANIPULASI data yang ada pada sebuah DATABASE.
