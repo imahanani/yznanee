9bc52d00187e5bda352954fe1ddb70caa5d735ed
Yznanee <115205561+yznanee@users.noreply.github.com>
2023-04-08 15:56:56 +0700

    Delete PostgreSQL.sql

--- PostgreSQL.sql
+++
@@ -1,6 +0,0 @@
-SQL kepanjangannya apa?
-SQL adalah singkatan dari Structured Query Language.
-http://sistem-komputer-s1.stekom.ac.id/informasi/baca/Apa-Itu-SQL-Pengertian-Jenis-Beserta-Fungsinya 
-
-Perintah DDL : mendefinisikan atau membuat STRUKTUR BASIS DATA beserta tabelnya.
-Perintah DML : MEMANIPULASI data yang ada pada sebuah DATABASE.
