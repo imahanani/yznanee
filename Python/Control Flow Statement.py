4c59038c31c867bccb074290ee78b8b6793eb6b3
Yznanee <115205561+yznanee@users.noreply.github.com>
2023-04-11 13:27:19 +0700

    #branching #looping
    istirahat dulu, tabung tenaga buat sampai maghrib

--- Python/Control Flow Statement.py
+++ Python/Control Flow Statement.py
@@ -35,9 +35,82 @@ elif n>=70:
 else:
     print("Maaf, Anda belum lulus")
 ```
-  
-  
-  
+    
+LOOPING (FOR LOOP)
+#for #while
+
+```py
+#list suhu dalam C
+celcius = [25, 30, 35, 40, 45]
+#konversi suhu dari C ke F
+for x in celcius:
+    fahrenheit = (x*9/5)+32
+    print('='*20)
+    print("Suhu dalam C:", x, "derajat")
+    print("Suhu dalam F:", fahrenheit, "derajat")
+```
+
+CARA KEDUA, DISIMPAN KE VARIABEL BARU DENGAN FUNGSI "append"
+```py
+celcius = [25, 30, 35, 40, 45]
+f = []
+for x in celcius:
+    convert = (x*9/5) + 32
+    f.append(convert)
+f
+```
+
+CARA KE3
+```py
+celcius = [25, 30, 35, 40, 45]
+f = [(x*9/5) + 32 for x in celcius]
+f
+```
+
+for LOOP + BRANCHING , jika input bernilai minus '-' (x absolut NOL), program muncul pesan 'error'
+
+CARA PERTAMA
+```py
+celcius = [-400, -325, 0, 10, -5, 15]
+f = []
+
+for x in celcius:
+    if x >= -273.15:
+        convert = (x*9/5)+32
+        f.append(convert)
+    else:
+        print("error: suhu (x) di bawah absolute nol")
+f
+```
+CARA KEDUA
+```py
+celcius = [-400, -325, 0, 10, -5, 15]
+f = [(x*9/5) + 32 for x in celcius if x >= -273.15]
+f
+```
+
+WHILE LOOP while
+```py
+# i adalah nilai yang akan diulang
+i = 1
+while i <= 5:
+    print(i)
+    i += 1 
+# tanda " +=" berfungsi menjumlahkan angka iterasi ke angka sebelumnya
+```
+WHILE LOOP + BRANCHING
+```py
+#menjumlahkan hanya bilangan genap.  %2 = 0
+
+
+
+
+
+
+
+
+
+
   
   
   
