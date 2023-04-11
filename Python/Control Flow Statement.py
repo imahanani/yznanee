533521b54eb0c2f01cc46b5163c815af5c895c4a
Yznanee <115205561+yznanee@users.noreply.github.com>
2023-04-11 12:16:22 +0700

    brancing
    #algoritma #pseudocode #branching #casestudy

---
+++ Python/Control Flow Statement.py
@@ -0,0 +1,43 @@
+Control Flow Statement pada Python
+
+KONSEP BRACHING
+
+Method 'if', satu kondisi
+```py
+n=85
+if n>= 70: #kondisi terpenuhi
+  print("Selamat! Anda Lulus")
+MENGHASILKAN OUTPUT
+
+n=61
+if n>= 70: #kondisi terpenuhi
+  print("Selamat! Anda Lulus")
+  TIDAK MENGHASILKAN OUTPUT APAPUN
+```
+
+Method 'if else', dua kondisi
+```py
+n=85
+if n>= 70:
+  print("Selamat! Anda lulus")
+else:
+  print("Maaf, Anda belum lulus")
+```
+
+Method 'if, elif, else) u/ lebih dari 2 kondisi
+```py
+if n>=90:
+    print("Nilai Anda A")
+elif n>=80:
+    print("Nilai Anda B")
+elif n>=70:
+    print("Nilai Anda C")
+else:
+    print("Maaf, Anda belum lulus")
+```
+  
+  
+  
+  
+  
+  
