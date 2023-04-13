92c9ac6876ac146391fd1d3d16625b425824c9be
Isgi <115205561+imahanani@users.noreply.github.com>
2023-04-13 23:05:51 +0700

    Resourceful Links ....
     about Analysis Data through Python.

---
+++ Project/Dicoding - Analisis dengan Python.py
@@ -0,0 +1,10 @@
+https://pandas.pydata.org/Pandas_Cheat_Sheet.pdf
+https://matplotlib.org/cheatsheets/_images/cheatsheets-1.png
+https://www.kaggle.com/code/themlphdstudent/cheat-sheet-seaborn-charts
+https://docs.streamlit.io/library/cheatsheet
+https://docs.streamlit.io/streamlit-community-cloud/get-started
+https://docs.streamlit.io/streamlit-community-cloud/get-started/deploy-an-app
+https://github.com/dicodingacademy/a555-dashboard/blob/main/requirements.txt
+https://github.com/rfajri27/a555-dashboard/blob/main/README.md
+https://www.dicoding.com/academies/555/discussions
+https://www.dicoding.com/academies/555/tutorials/31230/submission-guidance
