079441c5a1784fa5deda1ba40ab2ee45dac71785
Isgi <115205561+imahanani@users.noreply.github.com>
2023-05-13 17:57:06 +0700

    Update ATA Day 2.sql
    Adding some STATMENTS queries

--- SSMS/ATA Day 2.sql
+++ SSMS/ATA Day 2.sql
@@ -28,3 +28,44 @@ from [dbo].[EmployeeSalary]
 
 --Note: make sure you are on the right database
 
+/* Where Statement
+= , <>, <, >, Anda, Or, Like, Null, Not Null, In
+*/
+Select * 
+from [dbo].[EmployeeDemographics]
+Where FirstName = 'Jim'
+
+Select * 
+from 
+Where
+
+
+Select * 
+from [dbo].[EmployeeDemographics]
+Where Age >= 30; 
+
+Select * -- 2 statements has to value TRUE
+from [dbo].[EmployeeDemographics]
+Where Age <= 32 AND Gender = 'Male';
+
+Select * -- 2 statements has to value TRUE -- 1 of true value TRUE, will be return
+from [dbo].[EmployeeDemographics]
+Where Age <= 32 or Gender = 'Male';
+
+Select *  -- wildcard %, everything on the column LastName consist S will return
+from [dbo].[EmployeeDemographics]
+Where LastName like 'S%'
+
+Select *  -- wildcard %, everything on the column LastName consist S and o will return
+from [dbo].[EmployeeDemographics]
+Where LastName like 'S%o%' 
+
+Select * --return the column FirstName in NULL only 
+from [dbo].[EmployeeDemographics]
+Where FirstName is null
+
+Select * -- return only FirstName Jim and Michael
+from [dbo].[EmployeeDemographics]
+Where FirstName IN ('Jim', 'Michael')
+
+
